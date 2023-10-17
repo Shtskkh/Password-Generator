1f49f099222b3a313e7c7725a7e16cce64a79b4f
Alexey Shatskikh <shtskkh@gmail.com>
2023-10-17 17:26:23 +0700

    Улучшение script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const url = 'https://www.random.org/strings/';
 function getSettings () {
     return {
+        num: '1',
         len: document.querySelector('.len').value,
-        useDigits: document.querySelector('.digits').checked,
-        useUpperCase: document.querySelector('.upperCase').checked,
-        useLowerCase: document.querySelector('.lowerCase').checked,
+        digits: document.querySelector('.digits').checked,
+        upperalpha: document.querySelector('.upperCase').checked,
+        loweralpha: document.querySelector('.lowerCase').checked,
+        unique: 'on',
+        format: 'plain',
+        rnd: 'new'
     }
 }
 
-function useValidation (settings) {
+function isValid (settings) {
     if (settings.len < 1){
         alert ('Число симоловом не может быть меньше 1.')
         return 0;
@@ -17,45 +20,24 @@ function useValidation (settings) {
         alert ('Число симоловом не может быть больше 20.')
         return 0;
     }
-    if (!settings.useDigits && !settings.useLowerCase && !settings.useUpperCase){
+    if (!settings.digits && !settings.upperalpha && !settings.loweralpha){
         alert ('С выбранными настройками строка будет пустой, выберете хотя бы 1 настройку.')
         return 0;
     }
     return 1;
-
 }
 
+const url = 'https://www.random.org/strings/';
 async function getPassword (settings) {
-    if(settings.useDigits) {
-        settings.useDigits = 'on'
-    }
-    if(settings.useUpperCase) {
-        settings.useUpperCase = 'on'
-    }
-    if(settings.useLowerCase) {
-        settings.useLowerCase = 'on'
-    }
-    if(!settings.useDigits) {
-        settings.useDigits = 'off'
-    }
-    if(!settings.useUpperCase) {
-        settings.useUpperCase = 'off'
-    }
-    if(!settings.useLowerCase) {
-        settings.useLowerCase = 'off'
-    }
-    const urlGet = url+`?num=1&len=${settings.len}&digits=${settings.useDigits}&upperalpha=${settings.useUpperCase}&loweralpha=${settings.useLowerCase}&unique=on&format=plain&rnd=new`
-    const response = await fetch(urlGet);
+    const params = new URLSearchParams(settings).toString().replaceAll('true', 'on').replaceAll('false','off');
+    let response = (await fetch(url + '?' + params));
     document.querySelector('.password').textContent = await response.text();
 }
 
-
 const button = document.querySelector('.gen-pass');
-
 button.onclick = () => {
     const settings = getSettings();
-    const validation = useValidation(settings);
-    if (validation) {
-        getPassword(settings);
+    if (isValid(settings)) {
+        getPassword(settings).then(); // Заглушка
     }
 }
